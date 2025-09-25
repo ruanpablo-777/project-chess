160af00edfe9fee83bdcd4c51ae81fb1fbeba196
unknown <ToyaMON777@outlook.com>
2025-09-25 09:59:57 -0300

    tabuleiro done by js

--- chess/tabuleiro.js
+++ chess/tabuleiro.js
@@ -2,23 +2,39 @@ const list = document.querySelectorAll('.list')
 let controle = 0
 
 list.forEach((element, index) => {
-    console.log(element)
-    const boxWhite = document.createElement('div')
-    boxWhite.classList.add('box-white')
-    const boxBlack = document.createElement('div')
-    boxBlack.classList.add('box-black')
-
     for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {            
-            if(controle == 0) {
-                element.appendChild(boxWhite)
-                controle++
-            }else{
-                element.appendChild(boxBlack)
-                controle--
-            }
+        
+        const boxWhite = document.createElement('div')
+        boxWhite.classList.add('box-white')
+        const boxBlack = document.createElement('div')
+        boxBlack.classList.add('box-black')
+        
+        if (index % 2 == 0) {
+        if(controle % 2 ==0 ) {
             
+            element.appendChild(boxWhite)
+            console.log(`${index}+${i}`)
+            boxWhite.innerHTML = `${index},${i}`
+        } else {
+            element.appendChild(boxBlack)
+            boxBlack.innerHTML = `${index},${i}`
+
         }
+      } else {
+          if(controle % 2 == 1 ) {
+            
+            element.appendChild(boxWhite)
+            console.log(`${index}+${i}`)
+            boxWhite.innerHTML = `${index},${i}`
+        } else {
+            element.appendChild(boxBlack)
+            boxBlack.innerHTML = `${index},${i}`
+
+        }
+
+      }
+     controle++   
     }
+
 
 })
